bd7b15b382d924d06d05ff613722b6a50253a2a5
learntodroid <56079350+learntodroid@users.noreply.github.com>
2020-08-10 10:23:32 +1000

    correct the toast message bug in Alarm class

--- app/src/main/java/com/learntodroid/simplealarmclock/createalarm/DayUtil.java
+++ app/src/main/java/com/learntodroid/simplealarmclock/createalarm/DayUtil.java
@@ -3,7 +3,7 @@ package com.learntodroid.simplealarmclock.createalarm;
 import java.util.Calendar;
 
 public final class DayUtil {
-    public static final String toDay(int day) {
+    public static final String toDay(int day) throws Exception {
         switch (day) {
             case Calendar.SUNDAY:
                 return "Sunday";
@@ -20,6 +20,6 @@ public final class DayUtil {
             case Calendar.SATURDAY:
                 return "Saturday";
         }
-        return null;
+        throw new Exception("Could not locate day");
     }
 }
